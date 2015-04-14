9e5aa1d6b3f94663ef6d4cc539db6b600dad158a
bltnico <bellot.n@gmail.com>
2015-04-14 12:07:18 +0200

    fix pdo charset output

--- backend/pdo_connect.php
+++ backend/pdo_connect.php
@@ -3,7 +3,7 @@
 
     function pdoConnect() {
         try {
-            return new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME."", DB_USER, DB_PASSWORD);
+            return new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME."", DB_USER, DB_PASSWORD, array( PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         } catch (PDOException $e) {
             if(DEV) print "[PDO Error] : " . $e->getMessage() . "<br/>";
             die();
--- backend/prices.php
+++ backend/prices.php
@@ -3,8 +3,27 @@
     require_once "pdo_connect.php";
 
     $db = pdoConnect();
+    $result = array();
+
+    // $query_ref = "SELECT item.id AS id_item, reference.id AS id_reference, reference.name, reference.prix, reference.reservation, reference.stock, reference.date, reference.publish FROM item, reference WHERE item.id_produit = ".$id_produit;
+    // $query_ref .= " AND item.id_reference = reference.id";
+    // $query_ref .= " AND reference.publish ='Y' AND reference.date > '".time()."'";
+    // $query_ref .= " ORDER BY reference.date ASC;";
+
+    // $req = $db->prepare("SELECT item.id AS id_item, reference.id AD id_reference, referance.name, reference.prix, reference.reservation, reference.stock, reference.date, reference.publish
+    //     FROM item, reference
+    //     WHERE item.id_product = ?
+    //     AND item.id_reference = reference.id
+    //     AND reference.publish = 'Y' AND reference.date > {time()}
+    //     ORDER BY reference.date ASC");
+    //
+    // var_dump($req->execute(array(10)));
+    //
+    // var_dump($req->fetch());
 
     $req = $db->query("SELECT * FROM account");
-    var_dump($req->fetch());
-
+    while($data = $req->fetch(PDO::FETCH_ASSOC)) {
+        array_push($result, $data);
+    }
+    exit(json_encode($result));
 ?>
