a073927a4472c1b0635f8066b55aff29d1f022c2
bltnico <bellot.n@gmail.com>
2015-04-14 14:29:25 +0200

    Add JsonResponse

--- backend/prices.php
+++ backend/prices.php
@@ -1,9 +1,10 @@
 <?php
     require_once "config.php";
-    require_once "pdo_connect.php";
+    require_once "utils.php";
 
     $db = pdoConnect();
     $result = array();
+    $now = time();
 
     // $query_ref = "SELECT item.id AS id_item, reference.id AS id_reference, reference.name, reference.prix, reference.reservation, reference.stock, reference.date, reference.publish FROM item, reference WHERE item.id_produit = ".$id_produit;
     // $query_ref .= " AND item.id_reference = reference.id";
@@ -21,9 +22,23 @@
     //
     // var_dump($req->fetch());
 
-    $req = $db->query("SELECT * FROM account");
+    // $req = $db->query("SELECT * FROM account");
+
+    // $req = $db->prepare("SELECT item.id AS id_item, reference.id AS id_reference, referance.name, reference.prix, reference.reservation, reference.stock, reference.date, reference.publish
+    //     FROM item, reference
+    //     WHERE item.id_produit = ?
+    //     AND item.id_reference = reference.id
+    //     AND reference.publish = 'Y' AND reference.date > {$now}
+    //     ORDER BY reference.date ASC");
+    //
+    // $req->execute(array(5));
+    // var_dump($req);
+
+    $req = $db->query("SELECT * FROM vitrine");
+
     while($data = $req->fetch(PDO::FETCH_ASSOC)) {
         array_push($result, $data);
     }
-    exit(json_encode($result));
+
+    return JsonResponse($result);
 ?>
---
+++ backend/utils.php
@@ -0,0 +1,35 @@
+<?php
+    require_once "config.php";
+
+    /**
+     * Create a new pdo intent
+     *
+     * @return {Object} PDO
+     */
+    function pdoConnect() {
+        try {
+            return new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME."", DB_USER, DB_PASSWORD, array( PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        } catch (PDOException $e) {
+            if(DEV) print "[PDO Error] : " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    /**
+     * Create a json response
+     * if env is dev, return a pretty json
+     *
+     * @param {Array} $result
+     */
+    function JsonResponse($result) {
+        if(DEV) {
+            print "<pre>";
+            print json_encode($result, JSON_PRETTY_PRINT);
+            print "</pre>";
+            die();
+        } else {
+            exit(json_encode($result));
+        }
+    }
+    
+?>
