b9e701fee520e8cfd28cb5adebf6569872473c75
bltnico <bellot.n@gmail.com>
2015-04-14 11:09:04 +0200

    Add pdo connect

---
+++ backend/pdo_connect.php
@@ -0,0 +1,16 @@
+<?php
+    require_once "config.php";
+
+    function pdoConnect() {
+        try {
+            $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", DB_USER, DB_PASSWORD);
+        } catch (PDOException $e) {
+            if(DEV) {
+                print "[PDO Error] : " . $e->getMessage() . "<br/>";
+            }
+            die();
+        }
+    }
+
+    pdoConnect();
+?>
