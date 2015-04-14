1814ed405d926da76002997437d9c8ac942f53b3
bltnico <bellot.n@gmail.com>
2015-04-14 11:14:00 +0200

    Fix pdo connect

--- backend/pdo_connect.php
+++ backend/pdo_connect.php
@@ -3,7 +3,7 @@
 
     function pdoConnect() {
         try {
-            $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", DB_USER, DB_PASSWORD);
+            $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME."", DB_USER, DB_PASSWORD);
         } catch (PDOException $e) {
             if(DEV) {
                 print "[PDO Error] : " . $e->getMessage() . "<br/>";
