0d4cdee76d59f7890f84e1bc13e29d711140ba16
bltnico <bellot.n@gmail.com>
2015-04-14 11:02:32 +0200

    Update config

--- backend/config.php
+++ backend/config.php
@@ -1,3 +1,19 @@
 <?php
-    define('DEV', true);
+    /**
+     * App environnement
+     */
+    define("DEV", true);
+
+    /**
+     * Site domain
+     */
+    define("DOMAIN", DEV ? "mobile.cfm33.com" : "http://localhost");
+
+    /**
+     * Mysql configuration
+     */
+    define("DB", "");
+    define("DB_HOST", ""):
+    define("DB_USER", "");
+    define("DB_PASSWORD", "");
 ?>
