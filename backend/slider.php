cf719148d727e92ec6f7ddbd978eb292678c99fd
bltnico <bellot.n@gmail.com>
2015-04-15 09:18:43 +0200

    Add slider.php

---
+++ backend/slider.php
@@ -0,0 +1,14 @@
+<?php
+    require_once "utils.php";
+    isAllowedRequest();
+
+    $db = pdoConnect();
+    $result = array();
+
+    $req = $db->query("SELECT * FROM image_flash WHERE id_module = 1 AND module = 'slider' ORDER BY ordre");
+    while($data = $req->fetch(PDO::FETCH_ASSOC)) {
+        array_push($result, $data);
+    }
+
+    return JsonResponse($result);
+?>
